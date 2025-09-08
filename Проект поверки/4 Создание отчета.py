7adb4f38bda18d7191041d8e0bab7f2423368183
OrionTarab <nikita.tarabash@gmail.com>
2025-09-09 00:37:07 +0300

    Е7-20: - заполняет рандомом значения в пределах допуска; - формирует отчет поверки заполняет значения)

--- Проект поверки/4 Создание отчета.py
+++ Проект поверки/4 Создание отчета.py
@@ -1,10 +1,15 @@
 import MOKO
 from datetime import datetime
 
-#Region Формирование отчета:
+#Region Формирование отчета
+MOKO.Program('tree', 'set', f'select = Формирование отчета')
 
 
+MOKO.Program('Control', 'set', 'Save project report')
+MOKO.Program('Control', 'set', 'Save word report')
 
+
+MOKO.Program('tree', 'set', 'chosen=passed')
 #EndRegion Формирование отчета:
 
 
